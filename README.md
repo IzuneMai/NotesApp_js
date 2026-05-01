97fb2faa853277f419ff021d0aabef943bd850f9
IzuneMai <izunemai13@gmail.com>
2026-05-01 18:59:32 +0300

    create notes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NotesApp_js
+just first project
--- main.js
+++ main.js
@@ -1,24 +1,54 @@
 const notesEl = document.querySelector('.notes');
-const addBtn = document.querySelector('.note-add')
+const addBtn = document.querySelector('.note-add');
 
 
-function createNote(title, text){
+function createNote(title, text) {
     const noteEl = document.createElement('div');
     noteEl.classList.add('note');
     noteEl.innerHTML = `
         <div class='note-header'>
-            <p>${title}</p>
-            <div class = 'note-button'>
+            <p id='note-title'>${title}</p>
+            <input id = 'note-title-input' class = 'hidden'>
+            <div>
                 <button class = 'note-edit'><i class='fas fa-pen'></i></button>
                 <button class = 'note-del'><i class="fa fa-trash" aria-hidden="true"></i></button>
             </div>
         </div>    
-        <p id = 'note-description'>${text}</p>
+        <p id ='note-description'>${text}</p>
+        <textarea id = 'note-description-textarea' class = 'hidden'></textarea>
     `
+
+    const editBtn = noteEl.querySelector('.note-edit');
+    const deleteBtn = noteEl.querySelector('.note-del');
+    const titleEl = noteEl.querySelector('#note-title');
+    const descriptionEl = noteEl.querySelector('#note-description');
+    const titleInputEl = noteEl.querySelector('#note-title-input');
+    const descriptionAreaEl = noteEl.querySelector('#note-description-textarea');
+
+    editBtn.addEventListener('click', (e) => {
+        titleEl.classList.toggle('hidden');
+        descriptionEl.classList.toggle('hidden');
+        titleInputEl.classList.toggle('hidden');
+        descriptionAreaEl.classList.toggle('hidden');
+    });
+
+    deleteBtn.addEventListener('click', (e) => {
+        noteEl.remove();
+    });
+
+    titleInputEl.addEventListener('input', (e) => {
+        titleEl.innerText = e.target.value;
+    });
+
+    descriptionAreaEl.addEventListener('input', (e) => {
+        descriptionEl.innerText = e.target.value;
+    });
+
     return noteEl;
 };
 
-addBtn.addEventListener('click', (e) =>{
-    const el = createNote('Заголовок', 'Текст');
+addBtn.addEventListener('click', (e) => {
+    const el = createNote('Title', '<3');
     notesEl.appendChild(el);
-})
+});
+//<button class = 'note-edit'><i class="fa fa-check" aria-hidden="true"></i></button>
